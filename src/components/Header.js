f794105d7dde34311d49be81aa2fcea6f3675349
waleedsadeh <140826450+waleedsadeh@users.noreply.github.com>
2025-02-19 16:17:00 +0000

    setup firebase with react app hosting

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,16 @@ function Header() {
     return (
         <div className='Header'>
             <img src={logo} className='Header-logo' alt='logo'/>
-            <div className='link1'>
-
-            </div>
-            <div className='link2'>
-
+            <div className='Header-links'>
+                <ul>
+                    <li>Pricing</li>
+                    <li>Contact us</li>
+                    <li>Log in</li>
+                    <li>Sign up</li>
+                </ul>
             </div>
         </div>
+        
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
