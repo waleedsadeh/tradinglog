be55abbd66839044685bac5749a2e841f8e65301
waleedsadeh <140826450+waleedsadeh@users.noreply.github.com>
2025-02-23 19:03:17 +0000

    add a Function to force a hard refresh when a new version is available to remove cache file in the web browser

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
+// Function to force a hard refresh when a new version is available to remove cache file in the web browser
+const checkForUpdates = () => {
+  if ('servicesWorker' in navigator) {
+    navigator.serviceWorker.getRegistration().then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.update();
+      });
+    });
+    caches.keys().then((cacheNames) => {
+      cacheNames.forEach((cacheName) => {
+        caches.delete(cacheName);
+      });
+    });
+      window.location.reload(true);
+  }
+};
+
+window.addEventListener('load', () => {
+  setTimeout(checkForUpdates,2000); // wait 2 seconds before checking
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
